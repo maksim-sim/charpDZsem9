d4f9f4c7990511300a6c376629137be4ffa28d9e
maksim kireev <maksimkir987@gmail.com>
2023-11-20 21:00:26 +0300

    add task02

---
+++ task02/Program.cs
@@ -0,0 +1,17 @@
+﻿int ReadInt(string mes){
+    Console.Write(mes);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int PrintEvenNumbers(int M, int N)
+{
+    if (M == N) return M;
+    else return M + PrintEvenNumbers(M + 1, N);
+}
+
+
+int M = ReadInt("Введите значение M: ");
+int N = ReadInt("Введите значение N: ");
+
+Console.WriteLine($"Сумма натуральных чисел от {M} до {N}: {PrintEvenNumbers(M, N)}");
