20a3067e03157cb2df371f3d33ad9d54484f0310
maksim kireev <maksimkir987@gmail.com>
2023-11-20 21:00:15 +0300

    add task01

---
+++ task01/Program.cs
@@ -0,0 +1,23 @@
+﻿int ReadInt(string mes){
+    Console.Write(mes);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void PrintEvenNumbers(int M, int N)
+{
+    if (M > N){
+        Console.WriteLine();
+        return;
+    }
+
+    if (M % 2 == 0) Console.Write(M + "\t");
+    PrintEvenNumbers(M + 1, N);
+}
+
+
+int M = ReadInt("Введите значение M: ");
+int N = ReadInt("Введите значение N: ");
+
+Console.WriteLine($"Чётные числа от {M} до {N}:");
+PrintEvenNumbers(M, N);
